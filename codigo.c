b34ec7fc650942a54ba98283597d37defaef0b4e
abelbague <a.bague@alumnos.upm.es>
2019-04-04 22:04:36 +0200

    Update codigo.c
    He añadido do whiles para que repita las preguntas del inicio del menu en caso de no introducir los enteros correspondientes. Luego he tabulado y dejado margenes  para que sea lea mejor el codigo y ademas he modificado la condicion de la contraseña pero no consigo que funcione bien

--- codigo.c
+++ codigo.c
@@ -15,48 +15,61 @@ void main()
 	char id[20],clave[20];
 	
 	printf("\n\t > Bienvenido a TRANSFUTURE\n\n");
-	printf("\n\t Ya tienes cuenta? SI (1) \t NO (2): ");
-	scanf("%i", &a);
+	
+	do {
+		printf("\n\t Ya tienes cuenta? SI (1) \t NO (2): ");
+		scanf("%i", &a);
+	} while(a != 1 && a != 2);
 	
 	switch(a)
 	{
 		case 1:
-			printf ("\n Iniciar sesion como usuario(1) o como empleado(2)?");
-			scanf (" %i",&c);
+			do {
+				printf ("\n Iniciar sesion como usuario(1) o como empleado(2)?");
+					scanf (" %i",&c);
+			}while(c != 1 && c != 2);
+			
 			switch (c)
 			{
-            			case 1:
-                		printf("\n Usuario: ");
+            	case 1:
+                	printf("\n Usuario: ");
                 		scanf(" %[^\n]",id);
-                		printf("\n Clave: ");
-                		scanf (" %[^\n]",clave);
-                		printf ("MAS FUNCIONES PROXIMAMENTE");
-                		break;
-            			case 2:
-                		printf ("\n Introduce la contrasena de empleado: ");
+                	printf("\n Clave: ");
+                		scanf(" %[^\n]",clave);
+                	printf("\nMAS FUNCIONES PROXIMAMENTE");
+                	break;
+                	
+            	case 2:
+                	printf ("\n Introduce la contrasena de empleado: ");
                			scanf(" %[^\n]",clave);
-                		printf("MAS FUNCIONES PROXIMAMENTE");
-				break;
+                	printf("\nMAS FUNCIONES PROXIMAMENTE");
+                break;
+                
+                default:
+                	printf("Operacion no disponible\n");
 			}
 			break;
+			
 		case 2:
 			printf("\n > Identificate:\n");
 			usuario usuario1;
-			printf("Introduce tu nick de usuario");
-			scanf(" %[^\n]",usuario1.id);
-			printf("\n Introduce tu nombre:");
-			scanf (" %[^\n]",usuario1.nombre);
-			printf("\n Introduce tus apellidos");
-			scanf(" %[^\n]",usuario1.apellidos);
-			printf ("Introduce tu contrasena (debe ser menor de 20 caracteres): ");
-			scanf ("[^\n]",usuario1.clave);
-			while (b>=20){
-                		b=strlen (usuario1.clave);
-               			printf ("Error en la contrasena introducela de nuevo: \n");
-               			scanf (" %[^\n]",usuario1.clave);
+			printf("Introduce tu nick de usuario:\t");
+				scanf(" %[^\n]",usuario1.id);
+			printf("\nIntroduce tu nombre:\t");
+				scanf (" %[^\n]",usuario1.nombre);
+			printf("\nIntroduce tus apellidos:\t");
+				scanf(" %[^\n]",usuario1.apellidos);
+			printf ("\nIntroduce tu contrasena (debe tener entre 4 y 20 letras):\t");
+				scanf(" %[^\n]",usuario1.clave);
+			
+			b=strlen(usuario1.clave);
+			while (b>20 && b<4){
+               	printf ("Error en la contrasena introducela de nuevo: \n");
+           			scanf (" %[^\n]",usuario1.clave);
             		}
             printf("MAS FUNCIONES PROXIMAMENTE");
             break;
+            
         default:
         	printf("Operacion no valida\n");
            		
