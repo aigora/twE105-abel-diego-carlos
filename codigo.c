38805951d88179e0edfa4f71cdd78cbba75582ee
abelbague <a.bague@alumnos.upm.es>
2019-04-04 21:26:51 +0200

    Update codigo.c
    he cambiado el nombre de la variable clave y arreglado el switch

--- codigo.c
+++ codigo.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
 #include <string.h>
+
 typedef struct {
     char nombre[20];
     char apellidos[50];
     char id[20];
     int numero;
-    char contrasenna[20];
+    char clave[20];
 }usuario;
 
 void main()
 {
 	int a, b, c, d;
-	char id[20],contrasenna[20];
+	char id[20],clave[20];
 	
 	printf("\n\t > Bienvenido a TRANSFUTURE\n\n");
 	printf("\n\t Ya tienes cuenta? SI (1) \t NO (2): ");
@@ -20,20 +21,20 @@ void main()
 	switch(a)
 	{
 		case 1:
-			printf ("\n Inicias sesion como usuario(1) o como empleado(2)?");
+			printf ("\n Iniciar sesion como usuario(1) o como empleado(2)?");
 			scanf (" %i",&c);
 			switch (c)
 			{
             			case 1:
                 		printf("\n Usuario: ");
                 		scanf(" %[^\n]",id);
-                		printf("\n Contraseña: ");
-                		scanf (" %[^\n]",contrasenna);
+                		printf("\n Clave: ");
+                		scanf (" %[^\n]",clave);
                 		printf ("MAS FUNCIONES PROXIMAMENTE");
                 		break;
             			case 2:
-                		printf ("\n Introduce la contrasenna de empleado: ");
-               			scanf(" %[^\n]",contrasenna);
+                		printf ("\n Introduce la contrasena de empleado: ");
+               			scanf(" %[^\n]",clave);
                 		printf("MAS FUNCIONES PROXIMAMENTE");
 				break;
 			}
@@ -47,15 +48,18 @@ void main()
 			scanf (" %[^\n]",usuario1.nombre);
 			printf("\n Introduce tus apellidos");
 			scanf(" %[^\n]",usuario1.apellidos);
-			printf ("Introduce tu contraseña (debe ser menor de 20 caracteres): ");
-			scanf ("[^\n]",usuario1.contrasenna);
+			printf ("Introduce tu contrasena (debe ser menor de 20 caracteres): ");
+			scanf ("[^\n]",usuario1.clave);
 			while (b>=20){
-                		b=strlen (usuario1.contrasenna);
-               			printf ("Error en la contraseña introducela de nuevo: \n");
-               			scanf (" %[^\n]",usuario1.contrasenna);
+                		b=strlen (usuario1.clave);
+               			printf ("Error en la contrasena introducela de nuevo: \n");
+               			scanf (" %[^\n]",usuario1.clave);
             		}
+            break;
+        default:
+        	printf("Operacion no valida\n");
            		printf("MAS FUNCIONES PROXIMAMENTE");
-		break;
+			
 	}
 	
 	return 0;
