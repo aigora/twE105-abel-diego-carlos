e5fc339ac161715604038791436bb8cfc42eb4d6
dhsanchez <d.hsanchez@alumnos.upm.es>
2019-04-05 12:25:04 +0200

    Update codigo.c
    Añado un bucle do-while para que el programa continue hasta que el usuario quiera salir. Incluimos las librerias windows.h para usar la funcion Sleep y stdlib para poder usar el system(cls) (borrado de pantalla).

--- codigo.c
+++ codigo.c
@@ -1,27 +1,27 @@
 #include<stdio.h>
 #include <string.h>
-
+#include <windows.h>
+#include <stdlib.h>
 
 typedef struct {
     char nombre[20];
     char apellidos[50];
     char id[20];
-    int numero;
     char clave[20];
 }usuario;
 
 void main()
 {
-	int a, b, c, d;
+	int a, b, c, salida;
 	char id[20],clave[20];
-	
+	do{
+	system("cls");
 	printf("\n\t > Bienvenido a TRANSFUTURE\n\n");
-	
 	do {
-		printf("\n\t Ya tienes cuenta? SI (1) \t NO (2): ");
+		printf("\n\t Ya tienes cuenta? SI (1) \t NO (2) \t O deseas salir? (3): ");
 		scanf("%i", &a);
-	} while(a != 1 && a != 2);
-	
+	} while(a != 1 && a != 2&&a!=3);
+
 	switch(a)
 	{
 		case 1:
@@ -29,7 +29,7 @@ void main()
 				printf ("\n Iniciar sesion como usuario(1) o como empleado(2)?");
 					scanf (" %i",&c);
 			}while(c != 1 && c != 2);
-			
+
 			switch (c)
 			{
             	case 1:
@@ -38,19 +38,22 @@ void main()
                 	printf("\n Clave: ");
                 		scanf(" %[^\n]",clave);
                 	printf("\nMAS FUNCIONES PROXIMAMENTE");
+                	Sleep(2000);
                 	break;
-                	
+
             	case 2:
                 	printf ("\n Introduce la contrasena de empleado: ");
                			scanf(" %[^\n]",clave);
                 	printf("\nMAS FUNCIONES PROXIMAMENTE");
+                	Sleep(2000);
                 break;
-                
+
                 default:
                 	printf("Operacion no disponible\n");
+                	Sleep(2000);
 			}
 			break;
-			
+
 		case 2:
 			printf("\n > Identificate:\n");
 			usuario usuario1;
@@ -62,20 +65,24 @@ void main()
 				scanf(" %[^\n]",usuario1.apellidos);
 			printf ("\nIntroduce tu contrasena (debe tener entre 4 y 20 letras):\t");
 				scanf(" %[^\n]",usuario1.clave);
-			
+
 			b=strlen(usuario1.clave);
+			Sleep(2000);
 			while (b>20 && b<4){
                	printf ("Error en la contrasena introducela de nuevo: \n");
            			scanf (" %[^\n]",usuario1.clave);
             		}
             printf("MAS FUNCIONES PROXIMAMENTE");
+            Sleep(2000);
             break;
-            
+        case 3:
+            Sleep(2000);
+            break;
         default:
         	printf("Operacion no valida\n");
-           		
-			
+        	Sleep(2000);
 	}
-	
+	} while(a!=3);
+
 	return 0;
 }
