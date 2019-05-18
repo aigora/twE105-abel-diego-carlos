021b548a44e38b6b65eff6e1c09ac6ca6cb28763
abelbague <a.bague@alumnos.upm.es>
2019-05-18 18:05:27 +0200

    estetica del menu y cambios de rendimiento

--- codigo.c
+++ codigo.c
@@ -8,128 +8,122 @@
 
 int main()
 {
-	int a, b, c, flag, contador = 1, salida, pin, aux = 0;
+	int a, b, c, flag, contador = 3, salida, pin;
 	float distancia,precio;
 	char id[20],clave[20];
 	char *p1,*p2;
 	FILE *puntero;
 	Ruta r;
 	do{
-	system("cls");
-
-	printf("\n\t >> Bienvenido a TRANSFUTURE << \n\n");
-	do {
-		printf("\n  >>  Ya tienes cuenta? \n\n SI (1) \t NO (2) \t Salir (3): ");
-			scanf("%i", &a);
-	} while(a != 1 && a != 2 && a != 3);
-
-	switch(a)
-	{
-		case 1:
-			do {
-				system("cls");
-				printf ("\n  >>  Eres CLIENTE (1)  o  EMPLEADO (2)? ");
-					scanf (" %i",&c);
-			}while(c != 1 && c != 2);
-
-			switch (c)
-			{
-            	case 1:
-            	    system("cls");
-                	flag=iniciar_sesion(puntero);
-                    if (flag==1){
-                        printf("\nBienvenido!\n\n");
-                        distancia=comprobar_ruta(puntero);
-                        if (distancia==-1)
-                            break;
-                        precio=calcularprecio(puntero,distancia);
-                    }else if(flag==-1){
-                        printf("No esta registrado ese usuario; Regístrese antes de iniciar sesion.\n");
-                    }
-                	Sleep(3000);
-                	break;
-
-            	case 2:
-            		printf ("\n Introduce el pin secreto de la empresa: ");
-                   	scanf("%i",&pin);
-                   	system("cls");
-            	if (pin!=P)
+		system("cls");
+	
+		printf("\n\t >> Bienvenido a TRANSFUTURE << \n\n");
+		do {
+			printf("\n  >>  Ya tienes cuenta? \n\n  (1)Si | (2)No | (3)Salir ");
+				scanf("%i", &a);
+		} while(a != 1 && a != 2 && a != 3);
+	
+		switch(a)
+		{
+			case 1:
+				do {
+					system("cls");
+					printf ("\n  >>  Eres (1)cliente o (2)empleado ?");
+						scanf (" %i",&c);
+				}while(c != 1 && c != 2);
+	
+				switch (c)
 				{
-					do
+	            	case 1:
+	            	    system("cls");
+	                	flag=iniciar_sesion(puntero);
+	                    if (flag==1){
+	                        printf("\n  Bienvenido...\n\n");
+	                        distancia=comprobar_ruta(puntero);
+	                        if (distancia==-1)
+	                            break;
+	                        precio=calcularprecio(puntero,distancia);
+	                    }else if(flag==-1){
+	                        printf("  Este usuario no existe... (Regístrese antes de iniciar sesion)\n");
+	                    }
+	                	Sleep(3000);
+	                	break;
+	
+	            	case 2:
+	            		printf ("\n  Introduce el pin secreto de la empresa: ");
+	                   	scanf("%i",&pin);
+	                   	system("cls");
+	            	if (pin!=P)
 					{
-               			{    
-               				printf("El pin es incorrecto\tRecuerda que solo tienes tres intentos \t\t ");
-               				printf(" intento numero: %d",contador+1);
-               				printf ("\n Introduce el pin secreto de la empresa: ");
-                   			scanf("%i",&pin);
-                   			system("cls");
-               				contador++;
-               				Sleep(2000);
-						}
-                	}
-                	while ((pin!=P)&&(contador<3));
-                }
-                	if (pin == P)
-					{
-                        FILE *file=fopen("Distancias.txt","a"); //ruta en nuestro pc del archivo al que queremos anadir texto
-                        // Ruta en nuestro pc del archivo al que queremos anadir texto
-
-                        if(file == NULL)
-                        {
-                            printf("Error al abrir el archivo");
-                            //return 0;
-                        }
-						else
+						do
+						{   
+	               				printf("  El pin es incorrecto...");
+	               				printf ("\n  Introduce el pin secreto de la empresa\t (Te quedan %i intentos)",contador-1);
+	               				contador--;
+	                   			scanf("%i",&pin);
+	                   			system("cls");
+	               				Sleep(1000);
+	                	} while (pin!=P && contador>1);
+	                }
+	                	if (pin == P)
 						{
-                            do
+	                        FILE *file=fopen("Distancias.txt","a"); //ruta en nuestro pc del archivo al que queremos anadir texto
+	                        // Ruta en nuestro pc del archivo al que queremos anadir texto
+	
+	                        if(file == NULL)
+	                        {
+	                            printf("  Error al abrir el archivo...");
+	                            //return 0;
+	                        }
+							else
 							{
-                                system("cls");
-                                printf("Que desea hacer? 1.Introducir nueva ruta\t 2.Modificar precios\t 3.Salir \t");
-                            scanf ("%i",&b);
-                            fflush(stdin);
-                            switch (b)
-                            {
-                                case 1:
-                                nueva_ruta(file,r);
-                                break;
-                                case 2:
-                                    modificar_precios(puntero);
-                                break;
-                                case 3:
-                                flag=1;
-                                printf("\nHasta la proxima");
-                                break;
-                            }
-                            }
-							while(flag!=1);
-                        }
-                	}
-                	
-					else if(contador==3)
-					{
-                        printf("\n\tPor motivos de seguridad seras redirigido al menu de inicio\n");
-                        Sleep(3000);
-                        contador=0;
-                        break;
-                	}
-                	Sleep(2000);
-			}		
-                    break;
-            
-
-		case 2:
-			nuevousuario(puntero);
-            printf("\nSERA REDIRIGIDO AL HOME");
-            Sleep(2000);
-            break;
-        case 3:
-        	system("cls");
-        	printf("\n  >>  Hasta la proxima.");
-            break;
-        default:
-        	printf("Operacion no valida\n");
-        	Sleep(2000);
-	}
+	                            do
+								{
+		                            system("cls");
+		                            printf("  Que desea hacer?\n  (1)Introducir nueva ruta | (2)Modificar precios | (3)Salir");
+		                            	scanf ("%i",&b);
+		                            fflush(stdin);
+		                            switch (b)
+		                            {
+		                                case 1:
+		                                nueva_ruta(file,r);
+		                                break;
+		                                case 2:
+		                                    modificar_precios(puntero);
+		                                break;
+		                                case 3:
+		                                flag=1;
+		                                printf("\n  Hasta la proxima...");
+		                                break;
+		                            }
+	                            } while(flag!=1);
+	                        }
+	                	}
+	                	
+						else if(contador==1)
+						{
+	                        printf("\n  Por motivos de seguridad seras redirigido al menu de inicio...");
+	                        Sleep(3000);
+	                        contador=3;
+	                        break;
+	                	}
+	                	Sleep(1000);
+				}		
+	            break;
+	            
+			case 2:
+				nuevousuario(puntero);
+	            printf("\n  Estas siendo redirigido al inicio...");
+	            Sleep(2000);
+	            break;
+	        case 3:
+	        	system("cls");
+	        	printf("\n  >>  Hasta la proxima");
+	            break;
+	        default:
+	        	printf("Operacion no valida\n");
+	        	Sleep(2000);
+		}
 	} while(a!=3);
 
 	//return 0;
--- libreria.c
+++ libreria.c
@@ -24,11 +24,11 @@ int nuevousuario(FILE *pf){
         if (pf==NULL)
             printf("ERROR AL ABRIR EL ARCHIVO\n");
         else{
-            printf (" \nSi desea salir pulse 0, para continuar pulse 1 \n");
+            printf ("\n  Si desea salir pulse 0, para continuar pulse 1 \n");
             scanf("%i",&flag);
             fflush(stdin);
             if (flag==1){
-                printf ("Escribe tu usuario: ");
+                printf ("  Escribe tu usuario: ");
                 scanf ("%s",id);
                 _strupr(id);
                 do{
@@ -39,12 +39,12 @@ int nuevousuario(FILE *pf){
                 }while (feof(pf)==0&&flag2!=1);
                 fclose(pf);
                 if (flag2==0){
-                    printf ("\nIntroduzca una contrasena: ");
+                    printf ("\n  Introduzca una contrasena: ");
                     fflush(stdin);
                     scanf ("%s",contrasenna);
                     b=strlen(contrasenna);
                     while (b>20 || b<4){
-                        printf ("Error en la contrasena introducela de nuevo: \n");
+                        printf ("  Error en la contrasena, introducela de nuevo: \n");
                         scanf (" %[^\n]",contrasenna);
                         b=strlen(contrasenna);
                     }
@@ -52,17 +52,17 @@ int nuevousuario(FILE *pf){
                     fprintf(pf,"%s;%s;\n",id,contrasenna);
                     fclose (pf);
                     system ("cls");
-                    printf("ESCRITURA REALIZADA CON EXITO");
+                    printf("  ESCRITURA REALIZADA CON EXITO...");
                     salida=0;
                 }
                 else {
                     system("cls");
-                    printf("El usuario ya esta registrado, pruebe con un usuario distinto\n");
+                    printf("  Este usuario ya esta registrado, pruebe con un usuario distinto\n");
                 }
+        	}
         }
-        }
-    }while(flag!=0&&salida!=0);
-        Sleep(2000);
+    } while(flag !=0 && salida!=0);
+        Sleep(1000);
         return 0;
 }
 float comprobar_ruta(FILE *pf){
@@ -71,14 +71,14 @@ float comprobar_ruta(FILE *pf){
     char Origen[20],Destino[20],*p1,*p2;
     pf=fopen("Distancias.txt","r");
     if (pf==NULL)
-        printf("ERROR AL ABRIR EL ARCHIVO");
+        printf("  ERROR AL ABRIR EL ARCHIVO...");
     else{
-        printf("Introduzca el origen de su viaje:");
-        scanf("%[^\n]",Origen);
+        printf("  Introduzca el origen de su viaje:");
+        	scanf("%[^\n]",Origen);
         _strupr(Origen);
-        printf("\nIntroduzca el destino de su viaje:");
-        fflush(stdin);
-        scanf("%[^\n]",Destino);
+        printf("\n\n  Introduzca el destino de su viaje:");
+        	fflush(stdin);
+        	scanf("%[^\n]",Destino);
         _strupr(Destino);
         //EN ESTE BUCLE WHILE SE VA BUSCANDO QUE Ruta1.Origen COINCIDA CON EL ORIGEN INTRODUCIDO POR EL USUARIO
         while (feof(pf)==0&&(flag!=1||flag2!=1)){
@@ -91,7 +91,7 @@ float comprobar_ruta(FILE *pf){
             //SI FLAG VALE UNO, LOS STRING SON IGUALES; POR TANTO COMPROBAMOS SI EL DESTINO TAMBIEN COINCIDE
             if (flag==1){
                 system("cls");
-                printf("\nCOMPROBANDO DESTINO...");
+                printf("\n  COMPROBANDO DESTINO...");
                 p1=Destino;
                 p2=ruta1.Destino;
                 flag2=comprobar_usuario(p1,p2);
@@ -99,73 +99,74 @@ float comprobar_ruta(FILE *pf){
         }
         fclose(pf);
         if (flag!=1||flag2!=1){
-            printf("LA RUTA NO ESTA EN NUESTRA BASE DE DATOS\n");
+            printf("\n  LA RUTA NO ESTA EN NUESTRA BASE DE DATOS...");
             Sleep(2000);
             return -1;
         }
         else {
-         return ruta1.Distancia;
+        	return ruta1.Distancia;
         }
-
     }
-
 }
+
 int iniciar_sesion(FILE *pf){
     usuario user1;
     int flag=0,flag2=0,contador=0;
     char id[20],clave[20],*p1,*p2;
     pf=fopen("usuarios.txt","r");
+    
     if(pf==NULL)
-        printf("ERROR AL ABRIR EL ARCHIVO");
+        printf("  ERROR AL ABRIR EL ARCHIVO...");
     else{
         do{
-        if (contador!=0)
-            printf("\n Usuario o contrasegna incorrectos.\n Trate de iniciar sesion de nuevo\n");
-        printf("\n Usuario: ");
-        scanf(" %[^\n]",id);
-        _strupr(id);
-        fflush(stdin);
-        printf("\n Clave: ");
-        scanf("%[^\n]",clave);
-        fflush(stdin);
-        contador++;
-        while (feof(pf)==0&&(flag!=1||flag2!=1)){
-            fscanf(pf,"%[^;];%[^;];\n)",user1.id,user1.clave);
-            p1=id;
-            p2=user1.id;
-            flag=comprobar_usuario(p1,p2);
-            if (flag==1){
-                p1=clave;
-                p2=user1.clave;
-                flag2=comprobar_usuario(p1,p2);
-            }
-
-        }
+	        if (contador!=0)
+	            printf("\n  Usuario o contrasena incorrectos...\t Trate de iniciar sesion de nuevo\n");
+	        printf("\n  Usuario: ");
+	        	scanf(" %[^\n]",id);
+	        _strupr(id);
+	        	fflush(stdin);
+	        printf("\n  Clave: ");
+	        	scanf("%[^\n]",clave);
+	        	fflush(stdin);
+	        contador++;
+	        while (feof(pf)==0&&(flag!=1||flag2!=1)){
+	            fscanf(pf,"%[^;];%[^;];\n)",user1.id,user1.clave);
+	            p1=id;
+	            p2=user1.id;
+	            flag=comprobar_usuario(p1,p2);
+	            if (flag==1){
+	                p1=clave;
+	                p2=user1.clave;
+	                flag2=comprobar_usuario(p1,p2);
+	            }
+	        }
         }while (flag==0 && flag2==0 && contador<4);
         fclose(pf);
+        
         if(flag==1&&flag2==1){
             return 1;
         }else if(flag!=1){
-            printf("Quizas no esta registrado, trate de registrarse antes de iniciar sesion\n");
+            printf("  Quizas no esta registrado...\t Registrese antes de iniciar sesion\n");
             return -1;
         }else if(flag2!=1){
-            printf("La contraseña es incorrecta, pruebe de nuevo o registrese antes de iniciar sesion\n");
+            printf("  La contraseña es incorrecta...\t Pruebe de nuevo o registrese antes de iniciar sesion\n");
             return -1;
         }
+	}
 }
-}
+
 int nueva_ruta(FILE *pf,Ruta r){
-    printf("\nIntroduce el origen: ");
-    scanf("%s", &r.Origen);
+    printf("\n  Introduce el origen: ");
+    	scanf("%s", &r.Origen);
     _strupr (r.Origen);
-    printf("Introduce el destino: ");
-    scanf("%s", &r.Destino);
+    printf("  Introduce el destino: ");
+    	scanf("%s", &r.Destino);
     _strupr (r.Destino);
-    printf("Introduce la distancia: ");
-    scanf("%f", &r.Distancia);
+    printf("  Introduce la distancia: ");
+    	scanf("%f", &r.Distancia);
     fprintf(pf, "\n%s;%s;%f", r.Origen, r.Destino, r.Distancia);
     fclose(pf);
-    printf("\nRutas agnadidas correctamente.\n\n");
+    printf("\n  Rutas anadidas correctamente...\n\n");
     Sleep(2000);
 }
     float calcularprecio (FILE *pf,float distancia){
@@ -174,44 +175,48 @@ int nueva_ruta(FILE *pf,Ruta r){
         pf=fopen("precios.txt","r");
         fscanf(pf,"%f;%f;%f;",&km,&noche,&premium);
         fclose(pf);
+        
         do{
-            fflush(stdin);
-            printf("\nVa a realizar el viaje de noche?\n 1.SI\t2.NO:");
-            scanf("%i",&a);
+            	fflush(stdin);
+            printf("\n  Va a realizar el viaje de noche?\n  (1)Si | (2)No:");
+            	scanf("%i",&a);
             if (a==1)
                 precio=distancia*noche;
-        }while(a!=1&&a!=2);
+        }while(a!=1 && a!=2);
+        
         do{
-            printf("\nDesea tener el servicio premium (cargador,conexion a Internet,etc) en su viaje:\n 1.SI\t 2.NO:");
-            scanf("%i",&a);
-            fflush(stdin);
+            printf("\n  Desea tener el servicio premium (cargador,conexion a Internet,etc) en su viaje:\n (1)Si | (2)No ");
+            	scanf("%i",&a);
+            	fflush(stdin);
             if (a==1)
                 precio+=distancia*premium;
-        }while(a!=1&&a!=2);
+        }while(a!=1 && a!=2);
+        
         precio+=distancia*km;
-        printf("\nEl precio final de tu viaje seria de %f ",precio);
+        printf("\n  El precio final de tu viaje seria de %f €",precio);
         Sleep(5000);
         return precio;
 }
 void modificar_precios(FILE *pf){
     float km,premium,noche;
     pf=fopen("precios.txt","w");
+    
     if(pf==NULL)
-        printf("ERROR AL ABRIR EL ARCHIVO");
+        printf("ERROR AL ABRIR EL ARCHIVO...");
     else{
         system("cls");
-        printf("\nIntroduzca el nuevo precio por kilometraje:");
-        scanf("%f",&km);
-        fflush(stdin);
-        printf("\n Introduzca el nuevo precio por el servicio nocturno:");
-        scanf("%f",&noche);
-        fflush(stdin);
-        printf("\n Introduzca el nuevo precio por el servicio premium:");
-        scanf("%f",&premium);
-        fflush(stdin);
+        printf("\n  Introduzca el nuevo precio por kilometro (Actual = %f): ",km);
+        	scanf("%f",&km);
+        	fflush(stdin);
+        printf("\n  Introduzca el nuevo precio por el servicio nocturno (Actual = %f): ",noche);
+        	scanf("%f",&noche);
+        	fflush(stdin);
+        printf("\n  Introduzca el nuevo precio por el servicio premium (Actual = %f): ",premium);
+        	scanf("%f",&premium);
+        	fflush(stdin);
         fprintf(pf,"%f;%f;%f;",km,noche,premium);
         fclose(pf);
         system("cls");
-        printf("ACTUALIZACION DE LOS PRECIOS REALIZADA CON EXITO");
+        printf("  ACTUALIZACION DE LOS PRECIOS REALIZADA CON EXITO...");
     }
 }
